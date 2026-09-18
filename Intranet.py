4c248ac05fbcc7f99ebde94706630c1fbfdd3d3e
TS-Tec-Dev <ts.tec.dev@gmail.com>
2026-09-18 13:32:58 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- Intranet.py
+++ Intranet.py
@@ -13,6 +13,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import io
+import html as htmlmod
 
 # Tenta importar bibliotecas para manipulação e carimbo de PDFs
 try:
